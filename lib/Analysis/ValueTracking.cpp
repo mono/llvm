76c95fb372b238248eb6ce2a72efdd68a7c27eb6
Zoltan Varga <vargaz@gmail.com>
2011-12-27 01:47:28 +0100

    Add two mono specific flags in the form of custom metadata, which make it possible/easier for LLVM to hoist loads out of loops.

--- lib/Analysis/ValueTracking.cpp
+++ lib/Analysis/ValueTracking.cpp
@@ -1917,6 +1917,11 @@ bool llvm::isSafeToSpeculativelyExecute(const Instruction *Inst,
     const LoadInst *LI = cast<LoadInst>(Inst);
     if (!LI->isUnordered())
       return false;
+    //
+    // Mono extension, the metadata identifies loads which can't fail.
+    //
+    if (Inst->getMetadata("mono.nofail.load"))
+      return true;
     return LI->getPointerOperand()->isDereferenceablePointer();
   }
   case Instruction::Call: {
