dec84616ba48ab21814f36280b4c46ba7cc278da
Zoltan Varga <vargaz@gmail.com>
2013-11-04 02:54:31 +0100

    Add a 'mono' calling convention which allows us to pass an argument in a non-abi register.

--- include/llvm-c/Core.h
+++ include/llvm-c/Core.h
@@ -304,6 +304,7 @@ typedef enum {
   LLVMColdCallConv        = 9,
   LLVMWebKitJSCallConv    = 12,
   LLVMAnyRegCallConv      = 13,
+  LLVMMono1CallConv       = 14,
   LLVMX86StdcallCallConv  = 64,
   LLVMX86FastcallCallConv = 65
 } LLVMCallConv;
