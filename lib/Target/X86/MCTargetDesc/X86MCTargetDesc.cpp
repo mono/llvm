c9a88a73b85d56871ff0488430f0295adba8373d
Zoltan Varga <vargaz@gmail.com>
2014-04-07 16:19:30 -0400

    Fix the usage of createDefCfa in the x86 backend, it should receive a negative offset.

--- lib/Target/X86/MCTargetDesc/X86MCTargetDesc.cpp
+++ lib/Target/X86/MCTargetDesc/X86MCTargetDesc.cpp
@@ -287,7 +287,7 @@ static MCAsmInfo *createX86MCAsmInfo(const MCRegisterInfo &MRI, StringRef TT) {
   // Initial state of the frame pointer is esp+stackGrowth.
   unsigned StackPtr = is64Bit ? X86::RSP : X86::ESP;
   MCCFIInstruction Inst = MCCFIInstruction::createDefCfa(
-      nullptr, MRI.getDwarfRegNum(StackPtr, true), -stackGrowth);
+      nullptr, MRI.getDwarfRegNum(StackPtr, true), stackGrowth);
   MAI->addInitialFrameState(Inst);
 
   // Add return address to move list
