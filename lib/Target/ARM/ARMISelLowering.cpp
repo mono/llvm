f18ec1f5af9f64f7954e926290836eea8b632466
Zoltan Varga <vargaz@gmail.com>
2017-02-14 22:49:43 -0500

    [arm] Handle CallingConv::Mono in getEffectiveCallingConv (), previously it would be handled by the default branch, which would fall through to the next switch case because llvm_unreachable() was a noop in release builds.

--- lib/Target/ARM/ARMISelLowering.cpp
+++ lib/Target/ARM/ARMISelLowering.cpp
@@ -1606,6 +1606,8 @@ ARMTargetLowering::getEffectiveCallingConv(CallingConv::ID CC,
       return CallingConv::ARM_AAPCS_VFP;
     else
       return CallingConv::ARM_AAPCS;
+  case CallingConv::Mono:
+	  return CallingConv::Mono;
   }
 }
 
