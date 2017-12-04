a21fcae962adff8ebe7083948edf79ed5ac5a73f
Zoltan Varga <vargaz@gmail.com>
2017-12-04 14:38:16 -0500

    Add a jenkins build script.

---
+++ scripts/ci/run-jenkins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+echo "ENVIRONMENT:"
+env
+
+#COMMON_ACVARS="ac_cv_func_fstatat=no ac_cv_func_readlinkat=no ac_cv_func_futimens=no ac_cv_func_utimensat=no"
+
+LLVM_BASE_CONFIGURE_FLAGS="--enable-libcpp --enable-optimized --enable-assertions=no --disable-jit --disable-docs --disable-doxygen"
+#LLVM_BASE_CONFIGURE_ENVIRONMENT="$COMMON_ACVARS"
+
+mkdir -p build
+cd build
+../configure --prefix=$PWD/usr --enable-targets="arm arm64" $LLVM_BASE_CONFIGURE_FLAGS CXXFLAGS="-Qunused-arguments"
+make -j4
+make install
+mkdir tmp-bin
+cp usr/bin/{llc,opt,llvm-dis} tmp-bin/
+rm usr/bin/*
+cp tmp-bin/* usr/bin/
+rm usr/lib/libLTO.* usr/lib/*.dylib
+tar cvzf llvm-osx64-$GIT_COMMIT.tar.gz usr
