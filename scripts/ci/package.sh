c30e48f2639f4e549e52f749b7b00d216a4bc3da
Ludovic Henry <luhenry@microsoft.com>
2018-04-04 11:28:35 -0400

    Remove dependency on cmake

--- scripts/ci/package.sh
+++ scripts/ci/package.sh
@@ -7,18 +7,6 @@ set -e
 
 TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd )"
 
-if [ ! -d "tmp-cmake" ]; then
-mkdir tmp-cmake
-cd tmp-cmake
-wget https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz
-tar xf cmake-3.5.2-Linux-x86_64.tar.gz
-cd cmake-3.5.2-Linux-x86_64/bin/
-export PATH=`pwd`:$PATH
-else
-cd tmp-cmake/cmake-3.5.2-Linux-x86_64/bin
-export PATH=`pwd`:$PATH
-fi
-
 echo "Cloning Clang"
 
 cd $TOP_DIR/tools
