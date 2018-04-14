bfc3103b1499cd2dea045cb7e331fee562afb8b0
Ludovic Henry <ludovic@xamarin.com>
2018-04-13 23:05:58 -0400

    Merge master changes to debian packaging scripts

--- scripts/ci/package.sh
+++ scripts/ci/package.sh
@@ -7,54 +7,17 @@ set -e
 
 TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd )"
 
-if [ ! -d "tmp-cmake" ]; then
-mkdir tmp-cmake
-cd tmp-cmake
-wget https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz
-tar xf cmake-3.5.2-Linux-x86_64.tar.gz
-cd cmake-3.5.2-Linux-x86_64/bin/
-export PATH=`pwd`:$PATH
-else
-cd tmp-cmake/cmake-3.5.2-Linux-x86_64/bin
-export PATH=`pwd`:$PATH
-fi
-
-echo "Cloning Clang"
-
-cd $TOP_DIR/tools
- 
-if [ ! -d "clang" ]; then
-git clone https://github.com/llvm-mirror/clang.git
-cd clang
-git checkout f37acf6b6adf1cc6c103981332f6db7f2002dcdc
-fi
-
-mkdir -p $TOP_DIR/build
-
-cd $TOP_DIR/build
-
-export PKG_DIR=$TOP_DIR/build/mono-llvm-3.9
-
-echo "INSTALLING LLVM TO BUILD DIR"
-
-mkdir -p $PKG_DIR
-
-cp -r $TOP_DIR/scripts/ci/debian $PKG_DIR/debian
-
-cd $PKG_DIR
-
 echo "MAKING DEB"
 
-if [ $LLVM_TARGET = "ARMV7" ]; then
-dpkg-buildpackage -d -us -uc -aarmhf
-fi
+mkdir -p $TOP_DIR/build/mono-llvm-3.9
+pushd $TOP_DIR/build/mono-llvm-3.9
 
-if [ $LLVM_TARGET = "HOST" ]; then
+cp -r $TOP_DIR/scripts/ci/debian debian
+
 dpkg-buildpackage -d -us -uc
-fi
+
+popd # $TOP_DIR/build/mono-llvm-3.9
 
 echo "DONE"
-
-cd ../
 
 
