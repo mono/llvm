db85ab2aa004cd51712f0016ad0c9076333fd03e
Zoltan Varga <vargaz@gmail.com>
2018-04-24 23:15:15 -0400

    Add jenkins build script.

---
+++ scripts/ci/run-jenkins.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+echo "ENVIRONMENT:"
+env
+
+llvm_base_CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86;ARM;AArch64 -G Ninja"
+
+llvm64_CMAKE_FLAGS="$llvm_base_CMAKE_FLAGS"
+llvm32_CMAKE_FLAGS="$llvm_base_CMAKE_FLAGS -DLLVM_BUILD_32_BITS=On"
+
+mkdir -p build
+cd build
+cmake $llvm64_CMAKE_FLAGS -DCMAKE_INSTALL_PREFIX=$PWD/../usr64 ../
+ninja
+ninja install
+cd ..
+mkdir -p build32
+cd build32
+cmake $llvm64_CMAKE_FLAGS -DCMAKE_INSTALL_PREFIX=$PWD/../usr32 ../
+ninja
+ninja install
+cd ..
+
+rm -rf tmp-bin
+mkdir tmp-bin
+cp usr64/bin/{llc,opt,llvm-dis,llvm-config} tmp-bin/
+rm usr64/bin/*
+cp tmp-bin/* usr64/bin/
+
+rm -rf tmp-bin2
+mkdir tmp-bin2
+cp usr32/bin/llvm-config tmp-bin2
+rm usr32/bin/*
+cp tmp-bin2/* usr32/bin/
+# Don't need 32 bit binaries
+rm -f usr64/lib/libLTO.* usr64/lib/*.dylib usr32/lib/libLTO.* usr32/lib/*.dylib
+tar cvzf llvm-osx64-$GIT_COMMIT.tar.gz usr64 usr32
